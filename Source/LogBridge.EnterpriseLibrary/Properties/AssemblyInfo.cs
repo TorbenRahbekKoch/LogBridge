77c4f41ef7a9101d953a12a925a131728f964f34
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-09-10 11:13:47 +0200

    Update AssemblyInfo.cs and nuspecs.

--- Source/LogBridge.EnterpriseLibrary/Properties/AssemblyInfo.cs
+++ Source/LogBridge.EnterpriseLibrary/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LogBridge.EnterpriseLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Log Bridge for EnterpriseLibrary.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Software Passion")]
 [assembly: AssemblyProduct("LogBridge.EnterpriseLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
--- Source/LogBridge.Log4Net/Properties/AssemblyInfo.cs
+++ Source/LogBridge.Log4Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LogBridge.Log4Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Log Bridge for Log4Net.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Software Passion")]
 [assembly: AssemblyProduct("LogBridge.Log4Net")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
